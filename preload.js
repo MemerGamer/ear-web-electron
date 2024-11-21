d65184fc9090ab2296afe68a0440a64e3d4bf662
MemerGamer <kovacsbalinthunor53@gmail.com>
2024-11-21 17:48:22 +0200

    feat!: working on electron wrapper config

--- preload.js
+++ preload.js
@@ -1,5 +1,9 @@
 const { contextBridge } = require("electron");
-
+const path = require("path");
 contextBridge.exposeInMainWorld("api", {
   sendMessage: (msg) => console.log(msg),
+  navigate: (givenPath) => {
+    // window.location.href = `${path}.html`;
+    path.join(__dirname, `../res/${givenPath}.html`);
+  },
 });
